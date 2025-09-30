3d4f51ca8d5407a5b8e24424d6b0c489f368a41a
Sanved <sanvednarwadkar@gmail.com>
2025-09-30 17:34:18 +0530

    added warning for creating DB normally. We will use Alembic for further DB creation and migration

--- src/shared/db.py
+++ src/shared/db.py
@@ -1,33 +1,45 @@
+import asyncio
+import warnings
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from src.shared.config import settings
 from src.shared.utils import setup_logger
-import asyncio
 from src.file_service.models import Tenant, File
 from src.shared.base import Base
+import warnings
 
+warnings.simplefilter("always", DeprecationWarning)  # Show all DeprecationWarnings
+
+
+# Setup logger
 logger = setup_logger()
 
+# Database engine setup
 engine = create_async_engine(url=settings.file_repo_postgresql_url)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 logger.debug("DB engine and Sessionmaker is created")
 
 
+# Decorator for deprecation warnings
+def deprecated(func):
+    async def wrapper(*args, **kwargs):
+        warnings.warn(
+            f"{func.__name__} is deprecated and will be removed in the future. "
+            "Please use Alembic migrations to manage your database schema.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return await func(*args, **kwargs)
+
+    return wrapper
+
+
+# Apply the decorator to the create_db function
+@deprecated
 async def create_db():
     async with engine.begin() as conn:
-
         await conn.run_sync(Base.metadata.create_all)
         logger.debug("Tables Created")
-
-
-# We don't need delete here - can be defined in pytest testing scripts
-# async def delete_db():
-#     async with engine.begin() as conn:
-#         from src.file_service.models import Tenant, Base
-
-#         await conn.run_sync(Base.metadata.drop_all)
-#         # await conn.run_sync(Base.metadata.create_all)
-#         logger.debug("Tables Deleted")
 
 
 # For VSCode terminal
